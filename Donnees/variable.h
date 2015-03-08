312085ef85a13ac93290ee88f44bd08364fe42ad
Léo Gautheron <aje@users.noreply.github.com>
2015-03-08 16:57:24 +0100

    ajout de plusieurs fonctions
    voir le commit sur variable .c si plus d'information sont nécéssaires

--- Donnees/variable.h
+++ Donnees/variable.h
@@ -9,8 +9,10 @@
 #include <string.h>
 #include "xmalloc.h"
 #include "pile_domaines.h"
+#include "listeContrainte.h"
 #include "listes.h"
 
+extern int num_ligne; // recouvre la variable num_ligne du fichier lex.l
 
 typedef struct variable{
   int id; // Chaque variable possède un id unique (initialement à -1, valeur fixé lors du tri des variables)
@@ -63,4 +65,21 @@ void afficher_variable(variable var);
 // Procédure d'affectation d'un domaine à une liste de variables. On créer une pile de domaines contenant d et on met cette pile dans V->domaines
 void affecterDommaineDansVariables(variables V, domaine d);
 
+// met v2 apres v1 en modifiant v1. pour cela on change le pointeur suivant du dernier element de v1 et le pointeur precedent du premier de v2
+void concat_variables(variables* v1, variables* v2);
+
+// retourne un pointeur sur la variable portant ce nom si elle existe, sinon quitte le programme. on place aussi l'indice de cette variable dans *indiceVariable
+variable* retournerVariablePortantNom(variables V, char* nom, int* indiceVariable);
+
+// renvoi 0 si l'evaluation d'au moins un des arbres de contrainte dans variable->contraintes vaut 0
+// sinon renvoi 1
+// renvoi aussi 1 si la variable n'a pas de contrainte
+int verifier_les_contraintes(variable* variable);
+
+// pour chaque variable de V, on prend un sous-ensemble des contraintes de lc et on les met dans la liste des contraintes de cette variable.
+// une contrainte est mise dans la liste d'une variable
+//  - si la contrainte fait intervenir cette variable
+//  - si la contrainte ne fait pas intervenir de variable d'indice plus grand
+// si il reste des contraintes non asignee a une variable en fin de cette procedure, on les supprime
+void affecterContraintesDansVariables(variables V, listeContrainte* lc, int nombreVariable);
   #endif
