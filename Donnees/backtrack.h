44764f4ad0e1f534aa03f0fbf7ad4dc888a81e64
Léo Gautheron <aje@users.noreply.github.com>
2015-03-09 19:22:00 +0100

    Create backtrack.h

---
+++ Donnees/backtrack.h
@@ -0,0 +1,10 @@
+#ifndef _BACKTRACK
+#define _BACKTRACK
+
+#include "variable.h"
+
+void resolutionBacktrackUneSolution(variables V);
+
+void resolutionBacktrackToutesSolutions(variables V);
+
+#endif
