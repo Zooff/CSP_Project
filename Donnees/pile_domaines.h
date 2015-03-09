34569f27181232abe39211845f766736f038de11
Léo Gautheron <aje@users.noreply.github.com>
2015-03-09 07:34:22 +0100

    pile_domaines.h: correction faut orthographe

--- Donnees/pile_domaines.h
+++ Donnees/pile_domaines.h
@@ -14,6 +14,6 @@ pile_domaines creer_pile_domaines();
 void empiler(pile_domaines *p, domaine d);
 void depiler(pile_domaines *p);
 void afficher_pile(pile_domaines p);
-void vider_pile_domaines(pile_domaine p);
+void vider_pile_domaines(pile_domaines p);
 
 #endif
