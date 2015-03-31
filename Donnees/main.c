090501b40b3f4d7baa8b4160f95196cf50531617
aje <leo_g_autheron@hotmail.fr>
2015-03-31 20:49:56 +0200

    ajout de main.c

---
+++ Donnees/main.c
@@ -0,0 +1,116 @@
+#include <unistd.h>
+#include "variable.h"
+#include "backtrack.h"
+
+extern FILE* yyin; /* pour remplacer stdin par un fichier */
+extern FILE* fileToWrite;
+
+extern variables listeVariables;
+extern pile_domaines listeDomaines;
+
+int afficherContributeur = 0;
+int algorithmeUtilise = 1;
+int ecrireDansFichier = 0;
+int listeVariable = 0;
+int forwardChecking =0;
+
+extern void yylex_destroy();
+extern void yyparse();
+
+void affichageContributeur()
+{
+	
+}
+
+void usage(char* prog){
+	fprintf(fileToWrite, "Usage : %s [-a nombre] [-o fichier_où_ecrire] [-s] [-t nombre] [-v] [-z] fichier_contenant_le_CSP\n", prog);
+	fprintf(fileToWrite, "\t-a nombre : permet d'utiliser l'algorithme de numéro nombre, nombre peut prendre plusieurs valeurs :\n");
+	fprintf(fileToWrite, "\t\t1 : l'algorithme utilisé sera l'algorithme de backtrack (comportement de base si l'option n'est pas renseignée)\n");
+	fprintf(fileToWrite, "\t\tTO DO\n");
+	fprintf(fileToWrite, "\t-o fichier_où_ecrire : le résultat du CSP sera écrit dans le fichier fichier_où_ecrire\n");
+	fprintf(fileToWrite, "\t-s : active le mode silence\n");
+	fprintf(fileToWrite, "\t-t nombre : permet de résoudre le CSP de numéro nombre, nombre peut prendre plusieurs valeurs :\n");
+	fprintf(fileToWrite, "\t\t1 : le CSP résolu sera un CSP classique (comportement de base si l'option n'est pas renseignée)\n");
+	fprintf(fileToWrite, "\t\t2 : le CSP résolu sera un sudoku\n");
+	fprintf(fileToWrite, "\t\tTO DO\n");
+	fprintf(fileToWrite, "\t-v : affiche la liste des variables\n");
+	fprintf(fileToWrite, "\t-z : affiche les contributeurs du programme\n");
+	exit(EXIT_FAILURE);
+}
+
+int main(int argc, char* argv[])
+{
+	char c;
+	char* fileName;
+	fileToWrite = stderr;
+	typeCSP = 1;
+	modeSilence = 0;
+	opterr = 0; //on bloque les messages d'erreurs de getopt
+		
+	while((c = getopt(argc, argv, "a:o:st:vz")) != -1){
+		switch (c) {
+		case 'a':
+			algorithmeUtilise = atoi(optarg);
+			break;
+		case 'o':
+			ecrireDansFichier++;
+			fileToWrite = fopen(optarg, "w");
+			break;
+		case 's':
+			modeSilence++;
+			break;
+		case 't':
+			typeCSP = atoi(optarg);
+			break;
+		case 'v':
+			listeVariable++;
+			break;
+		case 'z' :
+			afficherContributeur++;
+			break;
+		default:
+			usage(argv[0]);
+		}
+	}
+		
+	if (optind < argc)
+		fileName = argv[optind];
+	else
+		usage(argv[0]);
+
+	//analyse du fichier
+    yyin=fopen(fileName, "r"); /* ouverture du fichier contenant le CSP */
+    yyparse();
+    fclose(yyin);
+    
+	// affichage resultat
+	if(listeVariable)
+	    afficher_liste(listeVariables);
+
+    initialiserValeurVariables(listeVariables);
+    
+    switch(algorithmeUtilise){
+	case 1 :
+		resolutionBacktrackUneSolution(listeVariables);
+		break;
+	case 2:
+		resolutionBacktrackToutesSolutions(listeVariables);
+		break;
+	case 3:
+		forwardChecking = 1;
+		resolutionBacktrackUneSolution(listeVariables);
+		break;
+	default :
+		fprintf(fileToWrite, "Numero d'algo de resolution inconnu\n");
+		usage(argv[0]);
+    }
+
+	if(afficherContributeur)
+		affichageContributeur();
+
+	// vidages
+    vider_pile_domaines(listeDomaines);
+    liberer_liste(listeVariables);
+    yylex_destroy();
+    return EXIT_SUCCESS;
+}
